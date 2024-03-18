f0ffcf6b3bea6e569cdcff6cc7586d52510ff3f3
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-03-18 11:57:58 +0300

    Tablolar Arası İlişkiler Eklendi

---
+++ BlogApp/BlogApp/Entity/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogApp.Entity;
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public DateTime PublishedOn { get; set; }
+    public bool IsActive { get; set; }
+    public int UserId { get; set; }
+
+    public User User { get; set; } = null!; // Navigation property -> Her postun bir kullanıcısı olmalı
+    public List<Tag> Tags { get; set; } = new List<Tag>(); //Her postun birçok tagı olabilir
+    public List<Comment> Comments { get; set; } = new List<Comment>(); //Her postun birçok yorumu olabilir
+}
