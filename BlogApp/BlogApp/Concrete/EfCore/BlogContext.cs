8f6b80cef2543d821ee7c7b680f10b4238d62f7f
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-03-18 12:12:05 +0300

    DbContext Eklendi

---
+++ BlogApp/BlogApp/Concrete/EfCore/BlogContext.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Concrete.EfCore
+{
+    public class BlogContext:DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options):base(options)
+        {
+        }
+        public DbSet<Post> Posts => Set<Post>();
+        public DbSet<User> USers => Set<User>();
+        public DbSet<Comment> Comments => Set<Comment>();
+        public DbSet<Tag> Tags => Set<Tag>();
+        
+    }
+}
