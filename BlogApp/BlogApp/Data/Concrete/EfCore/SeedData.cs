ffec3ca9f43fddc82bf303d5e72c1a952bb3508f
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-03-19 14:42:51 +0300

    Post Sayfası

--- BlogApp/BlogApp/Data/Concrete/EfCore/SeedData.cs
+++ BlogApp/BlogApp/Data/Concrete/EfCore/SeedData.cs
@@ -41,6 +41,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-10),
                             Tags = context.Tags.Take(3).ToList(), 
+                            Image = "1.jpg",
                             UserId = 1,
                         },
                         new Post()
@@ -50,6 +51,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-20),
                             Tags = context.Tags.Take(2).ToList(),
+                            Image = "2.jpg",
                             UserId = 1,
                         },
                         new Post()
@@ -59,6 +61,7 @@ namespace BlogApp.Data.Concrete.EfCore
                             IsActive = true,
                             PublishedOn = DateTime.Now.AddDays(-5),
                             Tags = context.Tags.Take(4).ToList(),
+                            Image = "3.jpg",
                             UserId = 2,
                         }
                     );
