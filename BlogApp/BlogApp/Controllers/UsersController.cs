22bc2c835c7a88490a9e29fd3fd20443d33e405f
hkmdrmuhamd <muhammedhkmdr@gmail.com>
2024-03-26 11:58:49 +0300

    Kullanıcı Profil Sayfası Yapıldı

--- BlogApp/BlogApp/Controllers/UsersController.cs
+++ BlogApp/BlogApp/Controllers/UsersController.cs
@@ -136,5 +136,24 @@ namespace BlogApp.Controllers
             }
             return View(registerViewModel);
         }
+
+        public IActionResult Profile(string username)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return NotFound();
+            }
+            var user = _userRepository
+                        .Users
+                        .Include(x => x.Posts)
+                        .Include(x => x.Comments)
+                        .ThenInclude(x => x.Post)
+                        .FirstOrDefault(x => x.UserName == username);
+            if (user is null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
     }
 }
